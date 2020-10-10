f31bfa49634cefddfdb1547570e511e1fadaebf0
starudream <justwangsheng@qq.com>
2020-10-10 23:38:56 +0800

    feat: add ping

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/starudream/clash-speedtest
 go 1.15
 
 require (
+	github.com/go-ping/ping v0.0.0-20201008161548-5f9dc3248bce
 	github.com/go-sdk/logx v1.1.6
 	github.com/go-sdk/utilx v1.0.5
 	github.com/olekukonko/tablewriter v0.0.4
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/go-ping/ping"
 	"github.com/go-sdk/logx"
 	"github.com/go-sdk/utilx/json"
 	"github.com/olekukonko/tablewriter"
@@ -38,6 +39,7 @@ type Dashboard struct {
 
 type Node struct {
 	Name  string  `json:"name"`
+	Ping  float64 `json:"ping"`  // ms
 	Speed float64 `json:"speed"` // kb/s
 }
 
@@ -163,40 +165,7 @@ func main() {
 
 		util.ProxySet(config.Proxy, config.Proxy)
 
-		result, node := &Result{}, &Node{Name: proxy.Name}
-
-		for j := int64(1); j <= config.Retry; j++ {
-			data, err := fast.GetData()
-			if err != nil {
-				logx.WithField("err", err).Errorf("[fast.com] api fail")
-				logx.Warnf("[speedtest] attempts %d time(s)", j)
-				time.Sleep(time.Second)
-				continue
-			}
-
-			logx.Infof("[%s] (%s) country: %s, city: %s", proxy.Name, data.Client.IP, data.Client.Location.Country, data.Client.Location.City)
-
-			if len(data.Targets) == 0 {
-				logx.Errorf("[%s] current area not exist speedtest node", proxy.Name)
-				break
-			}
-
-			target := data.Targets[0]
-
-			logx.Infof("[%s] speedtest node country: %s, city: %s", proxy.Name, target.Location.Country, target.Location.City)
-
-			result, err = SpeedTest(target.URL, config.Process)
-			if err != nil {
-				logx.WithField("err", err).Errorf("[%s] speedtest fail", proxy.Name)
-				continue
-			}
-
-			kb := float64(result.TotalBytes) / 1024
-			ti := float64(result.TotalTime) / float64(time.Second)
-			logx.Infof("[%s] speedtest download: %d kb, took: %.03f s, speed: %.02f kb/s", proxy.Name, int64(kb), ti, kb/ti)
-			node.Speed = kb / ti
-			break
-		}
+		result, speed := Speed(proxy)
 
 		util.ProxySet("", "")
 
@@ -204,21 +173,77 @@ func main() {
 			dashboard.TotalBytes += result.TotalBytes
 			dashboard.TotalTime += result.TotalTime
 		}
-		dashboard.Nodes[i] = node
+
+		dashboard.Nodes[i] = &Node{Name: proxy.Name, Ping: 0, Speed: speed}
 
 		logx.Infof("[%s] speedtest done, %d/%d", proxy.Name, i+1, len(names))
 	}
 
 	logx.Infof("total bytes: %.02f mb, total time: %d s", float64(dashboard.TotalBytes)/1024/1024, int64(dashboard.TotalTime/time.Second))
 
+	Print(dashboard.Nodes)
+}
+
+func Ping(addr string) float64 {
+	pinger, err := ping.NewPinger(addr)
+	if err != nil {
+		return 0
+	}
+	pinger.Timeout = 5 * time.Second
+	pinger.Count = 3
+	err = pinger.Run()
+	if err != nil {
+		return 0
+	}
+	stats := pinger.Statistics()
+	return float64(stats.AvgRtt) / float64(time.Millisecond)
+}
+
+func Speed(proxy clash.Proxy) (result *Result, speed float64) {
+	for j := int64(1); j <= config.Retry; j++ {
+		data, err := fast.GetData()
+		if err != nil {
+			logx.WithField("err", err).Errorf("[fast.com] api fail")
+			logx.Warnf("[speedtest] attempts %d time(s)", j)
+			time.Sleep(time.Second)
+			continue
+		}
+
+		logx.Infof("[%s] (%s) country: %s, city: %s", proxy.Name, data.Client.IP, data.Client.Location.Country, data.Client.Location.City)
+
+		if len(data.Targets) == 0 {
+			logx.Errorf("[%s] current area not exist speedtest node", proxy.Name)
+			return
+		}
+
+		target := data.Targets[0]
+
+		logx.Infof("[%s] speedtest node country: %s, city: %s", proxy.Name, target.Location.Country, target.Location.City)
+
+		result, err = SpeedTest(target.URL, config.Process)
+		if err != nil {
+			logx.WithField("err", err).Errorf("[%s] speedtest fail", proxy.Name)
+			continue
+		}
+
+		kb := float64(result.TotalBytes) / 1024
+		ti := float64(result.TotalTime) / float64(time.Second)
+		logx.Infof("[%s] speedtest download: %d kb, took: %.03f s, speed: %.02f kb/s", proxy.Name, int64(kb), ti, kb/ti)
+		speed = kb / ti
+		return
+	}
+	return
+}
+
+func Print(nodes []*Node) {
 	bb := &bytes.Buffer{}
 	writer := tablewriter.NewWriter(bb)
 	writer.SetAutoFormatHeaders(false)
 	writer.SetHeader([]string{"name", "speed(kb/s)"})
 	writer.SetFooter([]string{"name", "speed(kb/s)"})
 	writer.SetColumnAlignment([]int{tablewriter.ALIGN_LEFT, tablewriter.ALIGN_RIGHT})
-	for i := 0; i < len(names); i++ {
-		node, color := dashboard.Nodes[i], tablewriter.FgGreenColor
+	for i := 0; i < len(nodes); i++ {
+		node, color := nodes[i], tablewriter.FgGreenColor
 		if node.Speed < 1024 {
 			color = tablewriter.FgRedColor
 		} else if node.Speed < 3072 {
