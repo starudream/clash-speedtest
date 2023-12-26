d73449d4a4dc3c834a903ae572634f3d89efe32f
sheng.wang <justwangsheng@gmail.com>
2023-12-26 17:02:55 +0800

    docs: add speedtest size

--- api/speedtest/download.go
+++ api/speedtest/download.go
@@ -6,6 +6,14 @@ import (
 
 // size see: https://github.com/sivel/speedtest-cli/blob/v2.1.3/speedtest.py#L1186
 
+// 1000 ~ 1.99 MB
+// 1500 ~ 4.47 MB
+// 2000 ~ 7.91 MB
+// 2500 ~ 12.41 MB
+// 3000 ~ 17.82 MB
+// 3500 ~ 24.26 MB
+// 4000 ~ 31.63 MB
+
 const size = 1000 // ~ 2 MB
 
 func (c *Client) Download(s *Server, fn common.DownloadBodyFunc) (*common.DownloadResult, error) {
